8ed1ee1229c4a71692337454aa87bdc3e922f893
Juntian Zheng <j68zheng@uwaterloo.ca>
2021-04-29 17:28:51 -0400

    Added comments to Singly Linked List

--- LinkedList/SinglyLinkedList/SLinkedList.py
+++ LinkedList/SinglyLinkedList/SLinkedList.py
@@ -1,66 +1,91 @@
-# Definition of the node
-class ListNode: 
-    def __init__(self, val=0, next=None):
+class Node:
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
-        
-# Definition of the single-linked list        
+
 class MyLinkedList:
 
-    def __init__(self): 
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.size = 0
         self.head = None
         self.tail = None
-        self.length = 0
-		
+        
+
     def get(self, index: int) -> int:
-        if index < 0 or index >= self.length: return -1
-        else:
-            node = self.head
-            for _ in range(index):
-                node = node.next
-            return node.val
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if index < 0 or index >= self.size: return -1
+        
+        cur_node = self.head
+        for _ in range(index): 
+            cur_node = cur_node.next
+        
+        return cur_node.val
+        
 
     def addAtHead(self, val: int) -> None:
-        self.length += 1
-        if self.head is None:
-            self.head = self.tail = ListNode(val)
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        self.size += 1
+        if not self.head: self.head = self.tail = Node(val)
         else:
-            node = ListNode(val, self.head)
-            self.head = node
-   
-    def addAtTail(self, val: int) -> None: 
-        if self.head is None:
-            self.addAtHead(val)
+            node = self.head
+            self.head = Node(val, node)
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        if not self.head: return self.addAtHead(val)
         else:
-            self.length += 1
-            self.tail.next = ListNode(val)
+            self.size += 1
+            self.tail.next = Node(val)
             self.tail = self.tail.next
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index < 0 or index > self.length: return
-        if self.head is None or index == 0: return self.addAtHead(val)
-        if index == self.length: return self.addAtTail(val)
-        
-        self.length += 1
-        node = ListNode(val)
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        if index < 0 or index > self.size: return
+        if index == 0: return self.addAtHead(val)
+        if index == self.size:  return self.addAtTail(val)
+
+        self.size += 1
         prev = self.head
-        for _ in range(index-1): prev = prev.next
-        node.next = prev.next
-        prev.next = node
+        for _ in range(index - 1):
+            prev = prev.next
+        new_node = Node(val, prev.next)
+        prev.next = new_node
+        
 
     def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.length: return
-        self.length -= 1
-        if self.length == 0: 
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        if index < 0 or index >= self.size: return
+        
+        self.size -= 1
+        
+        # If the list will be empty, just set head and tail to None
+        if self.size == 0: 
             self.head = self.tail = None
             return
+        # If deleting head, just adjust self.head to its next
         if index == 0:
             self.head = self.head.next
             return
+        
         prev = self.head
-        for _ in range(index-1): prev = prev.next
+        for _ in range(index - 1):
+            prev = prev.next
         prev.next = prev.next.next
-        if self.length == index: self.tail = prev
+        
+        if index == self.size: self.tail = prev
 
 
 # Some driver code for testing
